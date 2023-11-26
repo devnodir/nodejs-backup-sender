8f7479dff6ca96bdaec648b40be11eab523bb11d
Abdunazarov Nodirbek <devnodir@gmail.com>
2023-11-26 20:06:03 +0500

    updated

--- app/main.ts
+++ app/main.ts
@@ -12,72 +12,71 @@ import fs from "fs/promises";
 
 class Main {
 	queueManager = new Queue();
-	bot = new Bot();
+	// bot = new Bot();
 	google = new Google();
 
 	async start() {
-		this.bot.init();
+		// this.bot.init();
 		const watcher = chokidar.watch(env.PathOfFolder, {
 			ignored: /(^|[\/\\])\../,
 			persistent: true,
-			awaitWriteFinish: true,
-			usePolling: true,
-			ignoreInitial: true,
-			alwaysStat: true
+			awaitWriteFinish: true
 		});
 		watcher.on("add", async (path, stats) => {
 			console.log("path", path);
 			console.log("stats", stats);
 			const result = await this.checkFile(path);
-			if (result) {
-				this.queueManager.enqueue(path);
-				if (this.queueManager.size() === 1) {
-					this.looper();
-				}
-			}
+			console.log(result);
+
+			// if (result) {
+			// 	this.queueManager.enqueue(path);
+			// 	if (this.queueManager.size() === 1) {
+			// 		this.looper();
+			// 	}
+			// }
 		});
 	}
 
-	async looper() {
-		while (this.queueManager.size() > 0) {
-			const item = this.queueManager.peek();
-			await this.sender(item);
-			this.queueManager.dequeue();
-		}
-	}
+	// async looper() {
+	// 	while (this.queueManager.size() > 0) {
+	// 		const item = this.queueManager.peek();
+	// 		await this.sender(item);
+	// 		this.queueManager.dequeue();
+	// 	}
+	// }
 
-	async sender(filePath: string) {
-		const filename = path.basename(filePath);
-		const fileSize = await stat(filePath).then((res) => res.size);
-		const humanSize = humanFileSize(fileSize);
+	// async sender(filePath: string) {
+	// 	const filename = path.basename(filePath);
+	// 	const fileSize = await stat(filePath).then((res) => res.size);
+	// 	const humanSize = humanFileSize(fileSize);
 
-		// try {
-		// 	await this.google.deleteOlderFiles();
-		// } catch (e) {
-		// 	await this.bot.sendError(e);
-		// }
+	// 	try {
+	// 		await this.google.deleteOlderFiles();
+	// 	} catch (e) {
+	// 		await this.bot.sendError(e);
+	// 	}
 
-		try {
-			const ids = await this.bot.sendingFile(filename, humanSize, "started");
+	// 	try {
+	// 		const ids = await this.bot.sendingFile(filename, humanSize, "started");
 
-			const fileId = await this.google.uploadFile(filePath, filename, fileSize).then((res) => res.data.id);
+	// 		const fileId = await this.google.uploadFile(filePath, filename, fileSize).then((res) => res.data.id);
 
-			const url = await this.google.generatePublicUrl(fileId).then((res) => res.data.webContentLink);
+	// 		const url = await this.google.generatePublicUrl(fileId).then((res) => res.data.webContentLink);
 
-			await this.bot.deleteLastMessage(ids);
+	// 		await this.bot.deleteLastMessage(ids);
 
-			await this.bot.sendingFile(filename, humanSize, "done", url);
+	// 		await this.bot.sendingFile(filename, humanSize, "done", url);
 
-			await this.sentToEmail(url);
+	// 		await this.sentToEmail(url);
 
-			await this.deleteCurrentFile();
-			console.info("Operation finished at", dayjs().format("DD.MM.YYYY - HH:mm"));
-		} catch (e) {
-			console.warn("Operation failed at", dayjs().format("DD.MM.YYYY - HH:mm"));
-			await this.bot.sendingFile(filename, humanSize, "failed");
-			await this.bot.sendError(e);
-		}
-	}
+	// 		await this.deleteCurrentFile();
+	// 		console.info("Operation finished at", dayjs().format("DD.MM.YYYY - HH:mm"));
+	// 	} catch (e) {
+	// 		console.warn("Operation failed at", dayjs().format("DD.MM.YYYY - HH:mm"));
+	// 		await this.bot.sendingFile(filename, humanSize, "failed");
+	// 		await this.bot.sendError(e);
+	// 	}
+	// }
 
 	sentToEmail(url: any) {
 		const transporter = mailer.createTransport({
